6cc4051fb93337135ae8a5e474a874684fdf172c
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-07-16 14:22:31 +0530

    Added messaging feature

--- src/components/Connect/index.jsx
+++ src/components/Connect/index.jsx
@@ -5,21 +5,43 @@ import {
   media,
   message,
 } from "../../styles/connect.module.css";
+import { db } from "../../firebase";
 
 function Connect() {
   const [bio, setBio] = useState(null);
+  const [by, setBy] = useState("");
+  const [email, setEmail] = useState("");
+  const [messageNew, setMessageNew] = useState("");
 
   useEffect(() => {
     const fetchData = () => {
       fetch("/biodata.json")
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
+          // console.log(data);
           setBio(data);
         });
     };
     fetchData();
   }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (email && by && message) {
+      db.collection("messages").add({
+        message: messageNew,
+        by: by,
+        email: email,
+        on: new Date(),
+        site: "https://imshahanwaz.web.app",
+        checked: false,
+      });
+      alert("Sent successfully!");
+      setBy("");
+      setEmail("");
+      setMessageNew("");
+    } else alert("Fill empty spaces first to send the message.");
+  };
 
   return (
     <div className={connect}>
@@ -66,10 +88,25 @@ function Connect() {
       <span>- OR -</span>
       <p>For any query or suggestions, direct message me here:</p>
       <div className={message}>
-        <form>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="for@example.com" />
-          <textarea type="text" placeholder="Write here..."></textarea>
+        <form onSubmit={sendMessage}>
+          <input
+            type="text"
+            placeholder="Name"
+            value={by}
+            onChange={(e) => setBy(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="for@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <textarea
+            type="text"
+            placeholder="Write here..."
+            value={messageNew}
+            onChange={(e) => setMessageNew(e.target.value)}
+          ></textarea>
           <button type="submit">
             <i className="bi bi-chat-left-quote-fill"></i>
             <p>Send</p>
